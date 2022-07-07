71c07ab8e9bfba03f73c795826855dc793c50717
ribamarf01 <ribamar.f01@gmail.com>
2022-07-07 18:55:25 -0300

    fixed null ref error

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,76 +5,76 @@ import SpriteWalk from "./SpriteWalk"
 const Hero = () => {
 
   const [walkNpcs, setWalkNpcs] = useState(false)
-  const mainRef = useRef<HTMLDivElement>(null)
+  const mainRef = useRef<HTMLDivElement>()
 
   useEffect(() => {
     setWalkNpcs(true)
-  })
+  }, [])
 
   const Sprites = <section>
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-1.gif"
-    spriteSpeed={40}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Farmboy */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-2.gif"
-    spriteSpeed={40}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Farmgirl */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-3.gif"
-    spriteSpeed={80}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Old man */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-4.gif"
-    spriteSpeed={25}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Children Redhair */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-5.gif"
-    spriteSpeed={50}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Man */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-6.gif"
-    spriteSpeed={45}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Woman */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-7.gif"
-    spriteSpeed={70}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Old woman */}
-  <SpriteWalk
-    spriteUrl="/img/npc/npc-walk-8.gif"
-    spriteSpeed={25}
-    initialDirection='right'
-    mainHeight={mainRef.current.offsetHeight}
-  />
-  {/* Blonde child */}
-    </section>
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-1.gif"
+      spriteSpeed={40}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Farmboy */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-2.gif"
+      spriteSpeed={40}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Farmgirl */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-3.gif"
+      spriteSpeed={80}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Old man */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-4.gif"
+      spriteSpeed={20}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Children Redhair */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-5.gif"
+      spriteSpeed={50}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Man */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-6.gif"
+      spriteSpeed={45}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Woman */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-7.gif"
+      spriteSpeed={70}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Old woman */}
+    <SpriteWalk
+      spriteUrl="/img/npc/npc-walk-8.gif"
+      spriteSpeed={25}
+      initialDirection='right'
+      mainHeight={mainRef.current ? mainRef.current.offsetHeight : 0}
+    />
+    {/* Blonde child */}
+  </section>
 
-  return <main className="min-h-screen w-full bg-grass relative overflow-hidden" ref={mainRef}>
-    { walkNpcs ? 
-      Sprites  
+  return <main className="min-h-screen w-full bg-grass relative overflow-hidden grid place-items-center" ref={mainRef}>
+    {walkNpcs ?
+      Sprites
       :
-      ''  
+      ''
     }
   </main>
 }
