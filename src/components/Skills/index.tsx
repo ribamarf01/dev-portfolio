73f0491809703ccd7b549b5a3a2bfea1f70fee31
ribamarf01 <ribamar.f01@gmail.com>
2022-07-08 22:45:28 -0300

    Skills small changes

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -2,7 +2,7 @@ const Skills = () => {
 
   return <main className="flex flex-col items-center p-8 bg-xiketic font-pixellari text-white">
     <h1 className="text-4xl text-center pb-4">Skills</h1>
-    <div className="flex justify-around w-1/3 h-[64px] bg-grass rounded-xl">
+    <div className="flex justify-around md:w-1/3 w-3/4 h-[64px] bg-grass rounded-xl">
       <img className="w-[64px] h-[64px]" src="/img/me/bow.gif" alt="My avatar shooting from a bow gif" />
       <img className="w-[64px] h-[64px] -scale-x-100" src="/img/target.gif" alt="A target being hitted by and arrow" />
     </div>
