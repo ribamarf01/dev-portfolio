e4036651ecbc93454bb78158ac2bb509646b3d27
ribamarf01 <ribamar.f01@gmail.com>
2022-07-10 23:06:52 -0300

    Mobile view for skills

--- src/components/Skills/SkillList.tsx
+++ src/components/Skills/SkillList.tsx
@@ -47,21 +47,29 @@ const SKILL_LIST: Skill[] = [{
 const SkillList = () => {
 
   const [content, setContent] = useState<Skill>()
+  const [revealedIndex, setRevealedIndex] = useState<number>(null)
+
+  const handleReveal = (index: number) => {
+    if(revealedIndex === index) setRevealedIndex(null)
+    else setRevealedIndex(index)
+  }
   
-  return <div className="flex items-center w-2/3 text-xl">
+  return <div className="flex items-center w-3/4 text-xl">
     <div className="flex items-center justify-around w-full text-white gap-x-8">
-      <ul className={`flex flex-col w-1/2 border rounded-lg p-4 h-[75vh] overflow-y-auto bg-paper text-independence`}>
-        { SKILL_LIST.map((skill, index) => (
-          <li className="flex flex-row flex-none items-center border-b border-b-independence py-2 cursor-pointer relative group overflow-hidden" onMouseEnter={() => setContent(skill)} onMouseLeave={() => setContent(null)} key={index} >
+      <ul className={`flex flex-col lg:w-1/2 w-full border rounded-lg p-4 h-[75vh] overflow-y-auto bg-paper text-independence`}>
+        { SKILL_LIST.map((skill, index) => (<div  key={index}>
+          <li className="flex flex-row flex-none items-center py-2 cursor-pointer relative group overflow-hidden" onClick={() => handleReveal(index)} onMouseEnter={() => setContent(skill)} onMouseLeave={() => setContent(null)} >
             <img className="h-16 w-16" src={skill.imgUrl} alt={skill.name} />
             <h1 className="w-full ml-4 text-2xl">{skill.name}</h1>
             <h2>{skill.rating}</h2>
-            <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
+            <svg className={`w-7 h-7 ${revealedIndex === index ? 'rotate-90': ''}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
             <div className="absolute top-0 -inset-full h-full w-1/2 z-5 block transform -skew-x-12 bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine"></div>
           </li>
-        )) }
+          <p className={`text-justify ${revealedIndex === index ? 'block': 'hidden'}`}>{skill.description}</p>
+          <div className="border-b border-b-independence"></div>
+          </div>)) }
       </ul>
-      <div className="w-1/2 h-[50vh] bg-paper text-independence rounded-lg">
+      <div className="w-1/2 h-[50vh] bg-paper text-independence rounded-lg lg:block hidden">
         { content ? 
           <div className="flex flex-col items-center h-full justify-center p-4">
             <h1 className="text-3xl pb-2">{content.name}</h1>
@@ -71,7 +79,7 @@ const SkillList = () => {
 
           </div> 
           :
-          <h1 className="text-3xl flex items-center justify-center h-full">Hover over to know more about the skill!</h1> 
+          <h1 className="text-3xl flex items-center text-center justify-center h-full">Hover over to know more about the skill!</h1> 
         }
       </div>
     </div>
