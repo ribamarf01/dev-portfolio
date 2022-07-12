deb087a382220c85635b6ce20523b11060ef0f7a
ribamarf01 <ribamar.f01@gmail.com>
2022-07-12 15:16:36 -0300

    fix firefox not bg-blur effect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,10 +20,10 @@ const Header = () => {
       <svg onClick={() => menuAction()} className="w-6 h-6 md:hidden mr-4 text-white transition-colors duration-300 cursor-pointer" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
     }
     <ul className={`text-rhythm md:text-3xl text-2xl font-pixellari md:flex md:static ${openMenu ? 'block absolute' : 'hidden'} md:top-0 top-16 gap-8 mr-4 text-center md:w-auto w-full md:bg-transparent md:backdrop-blur-0 md:shadow-none ${openMenu ? styles.glassEffectMenu : ''}`}>
-      <li className='md:py-0 py-2 text-white hover:text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>About me</li>
-      <li className='md:py-0 py-2 text-white hover:text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Skills</li>
-      <li className='md:py-0 py-2 text-white hover:text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Projects</li>
-      <li className='md:py-0 py-2 text-white hover:text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Contact</li>
+      <li className='md:py-0 py-2 md:text-white text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>About me</li>
+      <li className='md:py-0 py-2 md:text-white text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Skills</li>
+      <li className='md:py-0 py-2 md:text-white text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Projects</li>
+      <li className='md:py-0 py-2 md:text-white text-independence md:hover:bg-transparent hover:bg-gray-400 duration-300 transition-colors cursor-pointer'>Contact</li>
     </ul>
   </header>
   )
