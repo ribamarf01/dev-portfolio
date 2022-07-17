3daa6de584f96bbb45f125cf448b9cc00fcda8c5
ribamarf01 <ribamar.f01@gmail.com>
2022-07-17 12:55:26 -0300

    curriculum download button

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,7 @@ module.exports = {
         "rhythm": "#797596ff",
         "xiketic": "#171219ff",
         "play-button": "#45e41f",
-        "play-button-active": '#4db947',
-        "paper-sheet": "#d3bc8f"
+        "play-button-active": '#4db947'
       },
       backgroundImage: {
         "grass": "url('/img/grass.png')",
