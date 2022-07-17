52403e28cd044476ba73e8075bf2d8ab85eddbcf
ribamarf01 <ribamar.f01@gmail.com>
2022-07-17 13:04:15 -0300

    footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return <div className="bg-xiketic px-8 font-pixellari text-white flex flex-col items-center">
+    <p className="text-lg p-4 border-t-2">Ribamar Filho - 2022</p>
+  </div>
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import About from '@components/About'
 import Skills from '@components/Skills'
 import Projects from '@components/Projects'
 import Contact from '@components/Contact'
+import Footer from '@components/Footer'
 
 const Home = () => {
   return (<>
@@ -21,6 +22,7 @@ const Home = () => {
     <Skills />
     <Projects />
     <Contact />
+    <Footer />
   </>)
 }
 
